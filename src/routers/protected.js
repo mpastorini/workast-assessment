a7fe02dc1dda7e7a3eb2a863ba8b2bd4bd2d3a4e
mauromagicfitness <mauro.pastorini@magic.fitness>
2019-04-15 16:34:37 -0300

    Reduced unnecessary code

--- src/routers/protected.js
+++ src/routers/protected.js
@@ -3,11 +3,8 @@ require("express-async-errors"); // Technical Debt
 import ROUTES from "app/constants/routes";
 import { statusController } from "app/controllers";
 
-const router = express.Router();
-router
-  .get(ROUTES.API.SUBROUTES.PUBLIC.SUBROUTES.STATUS.ROUTE, statusController);
-
 /**
  * @description privated resources router
  */
-export default router;
+export default express.Router()
+  .get(ROUTES.API.SUBROUTES.PUBLIC.SUBROUTES.STATUS.ROUTE, statusController);
