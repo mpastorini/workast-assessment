42fc85c383ba97616ffd035ebf39cbadc26d97d3
mpastorini <mauro.h.pastorini@gmail.com>
2019-04-15 16:34:37 -0300

    Reduced unnecessary code

--- src/routers/public.js
+++ src/routers/public.js
@@ -2,9 +2,9 @@ import express from "express";
 require("express-async-errors"); // Technical Debt 
 import ROUTES from "app/constants/routes";
 import { statusController } from "app/controllers";
-const router = express.Router();
-router.get(ROUTES.API.SUBROUTES.PUBLIC.SUBROUTES.STATUS.ROUTE, statusController);
+
 /**
  * @description Public resources router
  */
-export default router;
+export default express.Router()
+  .get(ROUTES.API.SUBROUTES.PUBLIC.SUBROUTES.STATUS.ROUTE, statusController);
