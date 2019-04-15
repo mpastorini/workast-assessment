1e743f460649baa49b806ce00b07d3d6bff71765
mauromagicfitness <mauro.pastorini@magic.fitness>
2019-04-15 17:22:58 -0300

    Added env variables

--- src/middlewares/exception.js
+++ src/middlewares/exception.js
@@ -5,7 +5,11 @@ import { API_CONFIG } from "../constants";
  * @description Centralize exceptions thrown by express controllers
  */
 export default async function (error, request, response, next) {
+  if (API_CONFIG.ENVIRONMENT !== API_CONFIG.ENVIRONMENTS.TEST) console.error("Exception middleware: ", error);
+  if (API_CONFIG.ENVIRONMENT !== API_CONFIG.ENVIRONMENTS.DEVELOPMENT) delete error.stack;
   const errorString = (error && error.statusCode) ? error : API_CONFIG.INTERNAL_ERROR_MESSAGE;
+
   await response.status(error.statusCode || httpStatus.INTERNAL_SERVER_ERROR).json(errorString);
+
   next();
 }
