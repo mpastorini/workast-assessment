06007b016a9afa7b557730f44d6a78f8f5d2812f
mauromagicfitness <mauro.pastorini@magic.fitness>
2019-04-16 16:47:53 -0300

    Validate.js and User validator

--- src/constants/exceptions.js
+++ src/constants/exceptions.js
@@ -1,6 +1,19 @@
+/**
+ * @description Exception messages and codes
+ */
 export default {
   UNAUTHORIZED: {
     TOKEN_REQUIRED: "Token required",
     TOKEN_UNAUTHORIZED: "Invalid token"
+  },
+  BAD_REQUEST: {
+    INVALID_BODY: "INVALID_BODY",
+
+    USER: {
+      NAME_MUST_BE_STRING: "NAME_MUST_BE_STRING",
+      NAME_REQUIRED: "NAME_REQUIRED",
+      MESSAGE_MUST_BE_STRING: "MESSAGE_MUST_BE_STRING",
+      MESSAGE_REQUIRED: "MESSAGE_REQUIRED"
+    }
   }
 };
