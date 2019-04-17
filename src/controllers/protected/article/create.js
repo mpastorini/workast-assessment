260f9d2aab008e27eb7c29783643b8feae4fd98e
mpastorini <mauro.h.pastorini@gmail.com>
2019-04-16 22:50:15 -0300

    Code improvement over controllers

--- src/controllers/protected/article/create.js
+++ src/controllers/protected/article/create.js
@@ -4,5 +4,7 @@ import { createArticleService } from "app/services/articles";
  * @description Article creation route controller
  * @returns {Promise<{Article}>}
  */
-export default async (request, response) =>
-  response.json(await createArticleService(request.body));
+export default async (request, response) => {
+  const newArticle = await createArticleService(request.body);
+  return response.json(newArticle);
+};
--- src/controllers/protected/user/create.js
+++ src/controllers/protected/user/create.js
@@ -4,5 +4,7 @@ import { createUserService } from "app/services/users";
  * @description User creation route controller
  * @returns {Promise<{User}>}
  */
-export default async (request, response) =>
-  response.json(await createUserService(request.body));
+export default async (request, response) => {
+  const newUser = await createUserService(request.body);
+  return response.json(newUser);
+};
